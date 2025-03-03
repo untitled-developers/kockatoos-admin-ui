5901fc2a6e14cdc8014dc6cf418e2e836d8f9e76
mklsh <mostafaklch@hotmail.com>
2025-03-03 16:37:58 +0200

    updates

--- src/composables/useUtils.js
+++ src/composables/useUtils.js
@@ -69,10 +69,37 @@ export default function useUtils() {
         return `${year}-${month}-${day}`;
     }
 
+    function getTimeAgo(timeStamp) {
+        const date = new Date(timeStamp);
+        const now = new Date();
+        const seconds = Math.floor((now - date) / 1000);
+        const minutes = Math.floor(seconds / 60);
+        const hours = Math.floor(minutes / 60);
+        const days = Math.floor(hours / 24);
+        const months = Math.floor(days / 30);
+        const years = Math.floor(months / 12);
+
+        if (seconds < 60) {
+            return 'just now';
+        } else if (minutes < 60) {
+            return `${minutes} ${minutes === 1 ? 'minute' : 'minutes'} ago`;
+        } else if (hours < 24) {
+            return `${hours} ${hours === 1 ? 'hour' : 'hours'} ago`;
+        } else if (days < 30) {
+            return `${days} ${days === 1 ? 'day' : 'days'} ago`;
+        } else if (months < 12) {
+            return `${months} ${months === 1 ? 'month' : 'months'} ago`;
+        } else {
+            return `${years} ${years === 1 ? 'year' : 'years'} ago`;
+        }
+
+    }
+
     return {
         get,
         set,
         cloneDeep,
-        getFormattedDate
+        getFormattedDate,
+        getTimeAgo
     }
 }
