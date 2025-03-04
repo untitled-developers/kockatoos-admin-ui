c1c7cff0eb5293bcfb3225bd2fe4d43f849ababb
mklsh <mostafaklch@hotmail.com>
2025-03-04 15:01:17 +0200

    z-index fix

--- src/components/BaseAlert.vue
+++ src/components/BaseAlert.vue
@@ -1,12 +1,14 @@
 <template>
-  <Toast base-z-index="9999" :auto-z-index="false"></Toast>
+  <Toast :auto-z-index="false"></Toast>
 </template>
 
 <script setup>
 import {Toast} from "primevue";
-// Added the z-index stuff because when re-rendering a dialog ( to reset the state it's overlaying the toast component because of the auto layering)
+// Added the z-index stuff because when re-rendering a dialog ( to reset the state ) it's overlaying the toast component because of the auto layering
 </script>
 
 <style scoped>
-
+.p-toast {
+  z-index: 9999;
+}
 </style>
