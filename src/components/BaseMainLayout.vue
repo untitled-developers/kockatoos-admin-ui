e3a17b776b434b3dcc06f597db369d7612af2a96
mklsh <mostafaklch@hotmail.com>
2025-03-06 14:22:52 +0200

    responsive layout

--- src/components/BaseMainLayout.vue
+++ src/components/BaseMainLayout.vue
@@ -1,9 +1,77 @@
 <template>
+  <div class="card flex justify-center">
+    <Drawer v-model:visible="mobileDrawer" header="Menu">
+      <div class="flex flex-col w-full h-full">
+        <div>
+          <div class="card flex justify-center nav-menu">
+            <PanelMenu :model="navigationItems" multiple class="w-full md:w-80" :pt="{
+                root:{
+                  style: {
+                    borderRadius: 0,
+                    rowGap: 0,
+                  }
+                },
+                itemContent: {
+                  style: {
+                    borderRadius: 0
+                  },
+                  class: ['menu-sub-item']
+                },
+                headerContent:{
+                  style: {
+                    borderRadius: 0
+                  },
+                },
+                panel: {
+                  style: {
+                    border: 'none',
+                    borderRadius: '0',
+                    background: 'none',
+                    padding: 0
 
+                  }
+                },
+                rootList: {
+                  style: {
+                    padding: 0
+                  }
+                }
+
+              }">
+              <template #item="{ item , root, hasSubmenu }">
+                <router-link v-if="item.route" v-slot="{ navigate }" :to="item.route"
+                             @click="mobileDrawer = false"
+                             :active-class="isDark ? 'active-link-dark' : 'active-link'">
+                    <span
+                        :class="[
+                       'border-l border-gray-300 hover:border-gray-400 flex items-center cursor-pointer px-2 py-4',
+                        isDark ? 'text-white': 'text-surface-700',
+                        !root ? 'pl-6' : ''
+                  ]"
+                        @click="navigate">
+                        <span class="font-medium">{{ item.label }}</span>
+                    </span>
+                </router-link>
+                <div v-else
+                     :class="[
+  'flex items-center cursor-pointer px-2 py-4',
+  isDark ? 'text-white': 'text-gray-600',
+  !root ? 'pl-6' : ''
+]">
+                  <span class="font-medium">{{ item.label }} </span>
+                  <span v-if="item.items" class="pi pi-angle-down ml-auto"/>
+                </div>
+              </template>
+            </PanelMenu>
+          </div>
+        </div>
+      </div>
+    </Drawer>
+  </div>
   <div class="flex">
     <div>
       <nav v-if="!isCollapsed"
-           :class="['flex border-r  border-gray-300 w-64 shadow-md overflow-y-auto  h-screen sticky', isDark ? 'bg-stone-700':'bg-white']">
+           :class="['border-r  border-gray-300 w-64 md:flex hidden shadow-md overflow-y-auto  h-screen sticky', isDark ? 'bg-stone-700':'bg-white']">
         <div v-if="!isCollapsed" class="flex flex-col w-full h-full">
           <div class="flex w-full justify-center h-18 shrink-0  pl-4 py-2 pr-3 mb-4">
             <div>
@@ -80,7 +148,10 @@
     </div>
     <div class="flex-grow max-h-screen overflow-auto">
       <div class="px-2 py-2 pl-4 flex bg-white  border-b border-gray-300 shadow-md  mb-4 items-center">
-        <Button aria-label="Collapse navbar" text @click="handleToggleNavbar" icon="pi pi-bars"></Button>
+        <Button class="md:block hidden" aria-label="Collapse navbar" text @click="handleToggleNavbar"
+                icon="pi pi-bars"></Button>
+        <Button class="md:hidden block" aria-label="Collapse navbar" @click="mobileDrawer = true"
+                icon="pi pi-bars"></Button>
         <h2 class="text-lg font-bol text-gray-700">{{ route.meta?.title }}</h2>
         <div class="ml-auto flex items-center gap-x-2">
           <Button type="button"
@@ -119,6 +190,7 @@ import {ref} from "vue";
 import Button from "primevue/button";
 import Popover from "primevue/popover";
 import PanelMenu from "primevue/panelmenu";
+import {Drawer} from "primevue";
 import {useRoute} from "vue-router";
 
 const props = defineProps({
@@ -141,7 +213,7 @@ function handleToggleNavbar() {
   isCollapsed.value = !isCollapsed.value;
 }
 
-
+const mobileDrawer = ref(false)
 const userDropdown = ref();
 
 const toggle = (event) => {
