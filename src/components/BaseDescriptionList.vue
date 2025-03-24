cc520d66674560294391f14d62a033f8dcdb98d5
mklsh <mostafaklch@hotmail.com>
2025-03-24 16:56:10 +0200

    description listttt

---
+++ src/components/BaseDescriptionList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="px-4 sm:px-0">
+            <h3 class="text-lg font-semibold leading-7 text-gray-900">{{title}}</h3>
+            <p class="mt-1 max-w-2xl leading-6 text-gray-500">{{subtitle}}</p>
+        </div>
+        <div class="mt-6">
+            <dl :class="[gridColsClass]">
+                <slot></slot>
+            </dl>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {computed} from "vue";
+
+const props = defineProps({
+    title: String,
+    subtitle: String,
+    cols: {
+        type: Number,
+        default: 2
+    }
+})
+const gridColsClass = computed(() => {
+    return `grid grid-cols-1 sm:grid-cols-${props.cols}`
+})
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/BaseDescriptionListItem.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="border-t border-gray-100 px-4 py-6 sm:px-0" :class="[gridColsClass]">
+        <dt class=" font-medium leading-6 text-gray-900">
+            <slot name="label"></slot>
+        </dt>
+        <dd class="mt-1 leading-6 text-gray-700 sm:mt-2">
+            <Skeleton v-if="loading" width="10rem" height="1.2rem" class="mb-2"></Skeleton>
+            <slot v-else name="value"></slot>
+        </dd>
+    </div>
+</template>
+
+<script setup>
+import Skeleton from "primevue/skeleton";
+import {computed} from "vue";
+
+const props = defineProps({
+    cols: {
+        type: Number,
+        default: 1
+    },
+    loading: {
+        type: Boolean,
+        default: false
+    }
+})
+const gridColsClass = computed(() => {
+    return `col-span-${props.cols}`
+})
+
+</script>
+
+<style scoped>
+
+</style>
