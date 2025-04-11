fd2fec8c14bb2e51c20e4e2299b5c3f275067720
mklsh <mostafaklch@hotmail.com>
2025-04-11 15:29:44 +0300

    super edit dialogs

--- src/composables/useCreateFormSchema.js
+++ src/composables/useCreateFormSchema.js
@@ -1,23 +1,31 @@
+import * as zod from 'zod'
+
 export default function useCreateFormSchema({props}) {
     function createFormSchema(baseSchema, config = {
         languages: [],
         languageSchema: {}
     }) {
         let newSchema = baseSchema
-        if (baseSchema.shape['password']) {
-            if (props.record) {
-                newSchema = newSchema.omit({password: true})
-            }
+
+        // Handle password omission for existing records
+        if (baseSchema.shape['password'] && props.record) {
+            newSchema = newSchema.omit({password: true})
         }
+
+        // Handle languages
         if (config.languages.length > 0) {
+            const languageSchemaObject = zod.object({})
             config.languages.forEach(language => {
-                if (!newSchema.shape[language]) {
-                    newSchema = newSchema.extend({
-                        [language]: config.languageSchema
-                    })
-                }
+                languageSchemaObject.extend({
+                    [language]: config.languageSchema
+                })
             })
+            newSchema = newSchema.extend({
+                languages: languageSchemaObject
+            })
+
         }
+
         return newSchema
     }
 
