9569cf4804e24c9375a00908558baa88d1ea2d7a
mklsh <mostafaklch@hotmail.com>
2025-06-17 14:22:51 +0300

    2.0.0 update

---
+++ docs/assets/BasePageContent-3FPfhDDV.js
@@ -0,0 +1,1 @@
+import{_ as s,b as o,r as t,o as n}from"./index-DnFt5uzl.js";const r={},c={class:"px-4 py-2"};function a(e,_){return n(),o("div",c,[t(e.$slots,"default")])}const d=s(r,[["render",a]]);export{d as B};
